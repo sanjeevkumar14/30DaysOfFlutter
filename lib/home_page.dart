aa016fc99d2d6d4ef74abe53bba29402862800f3
Sanjeev Kumar <sanjeevkumar14102003@gmail.com>
2023-03-03 22:58:45 +0530

    First commit

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int days = 30;
+  var day = "Tuesday";
+  String name = "Sanjeev Kumar";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar( 
+          title: Text("Catalog App"),
+          ),
+        body: Center(
+          child: Text("Welcome to $days Day of Flutter by $name"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
