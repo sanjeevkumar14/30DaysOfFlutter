2de17cc7c00b75d338a046067ad04a4b2f453d9e
Sanjeev Kumar <sanjeevkumar14102003@gmail.com>
2023-03-04 23:30:24 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,14 +17,15 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(brightness: Brightness.dark),
       theme: ThemeData(
-      primarySwatch: Colors.amber
-      ),
-      initialRoute: "/home",
+          primarySwatch: Colors.amber,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          ),
+      initialRoute: "/login",
       routes: {
-        "/":(context) => HomePage(),
-        "/home":(context) => HomePage(),
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
       },
-   );
-   }
+    );
+  }
 }
